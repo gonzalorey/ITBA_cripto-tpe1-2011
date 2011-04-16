697a8586d5eb7824f7c5613cc773fa294c593721
anizzomc <anizzomc@6d05fb35-34e8-c98c-94b2-7deb9cabda7c>
2011-04-16 18:26:28 +0000

    Initial import.

---
+++ wavEncrypt/include/types.h
@@ -0,0 +1,15 @@
+/*
+ * types.h
+ *
+ *  Created on: Apr 15, 2011
+ */
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+typedef unsigned int DWORD;
+typedef unsigned short int WORD;
+typedef unsigned char BYTE;
+
+
+#endif /* TYPES_H_ */
