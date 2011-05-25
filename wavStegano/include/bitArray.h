506384f9f7fe95c227d8e86d139c3e39919233da
cliksmodernos <cliksmodernos@6d05fb35-34e8-c98c-94b2-7deb9cabda7c>
2011-05-25 23:55:06 +0000

    Added the partially implemented LSB4.

--- wavStegano/include/bitArray.h
+++ wavStegano/include/bitArray.h
@@ -13,4 +13,12 @@ int bitArrayGetDWORD(DWORD value, int idx);
 
 void bitArraySetBYTE(BYTE *value, int idx, int flag);
 
+int getFourLeastSignificantDWORD(DWORD value, int byte);
+
+int getFourMostSignificantDWORD(DWORD value, int byte);
+
+int bitArrayGetFourLeastSignificant(BYTE * array, int byte);
+
+int bitArrayGetFourMostSignificant(BYTE * array, int byte);
+
 #endif /* BITARRAY_H_ */
--- wavStegano/src/stegano.c
+++ wavStegano/src/stegano.c
@@ -14,10 +14,11 @@
 
 #define BITS_PER_BYTE 8
 #define BLOCK_SIZE 2
+#define BITS_PER_LSB4 4
 
 #define MAX_FILE_EXTENTION 8
 
-static int getPayloadSize(BYTE *carrier);
+static int getPayloadSizeLSB(BYTE *carrier);
 
 static void writePayloadSizeLSB(BYTE *carrier, int size);
 static void writePayloadDataLSB(BYTE *carrier, int offset, BYTE *payload, long bits);
@@ -27,6 +28,17 @@ static char getByteLSB(int *offset, BYTE *carrier);
 
 static stegResult_t stegEmbedLSB(dataHolder_t *carrier, dataHolder_t *payload, char *extention);
 static stegResult_t	stegExtractLSB(dataHolder_t *carrier, dataHolder_t *payload, char *extention);
+
+/*
+ * LSB4
+ */
+static stegResult_t stegEmbedLSB4(dataHolder_t *carrier, dataHolder_t *payload, char *extention);
+static stegResult_t stegExtractLSB4(dataHolder_t *carrier, dataHolder_t *payload, char *extention);
+
+static void writePayloadSizeLSB4(BYTE *carrier, int size);
+static void writePayloadDataLSB4(BYTE *carrier, int offset, BYTE *payload, long bits);
+static int getPayloadSizeLSB4(BYTE *carrier);
+static char getByteLSB4(int *offset, BYTE *carrier);
 
 stegResult_t stegEmbed(dataHolder_t *carrier, dataHolder_t *payload, stegMode_t mode, char *extention) {
 
@@ -80,7 +92,7 @@ static stegResult_t stegExtractLSB(dataHolder_t *carrier, dataHolder_t *payload,
 
 
 	LOG("reading payloadSize\n");
-	size = getPayloadSize(carrier->data);
+	size = getPayloadSizeLSB(carrier->data);
 	payload->size = size;
 	LOG("payload size: %d\n", size);
 
@@ -163,7 +175,7 @@ static void writePayloadDataLSB(BYTE *carrier, int offset, BYTE *payload, long b
 	}
 }
 
-static int getPayloadSize(BYTE *carrier) {
+static int getPayloadSizeLSB(BYTE *carrier) {
 	LOG("getPayloadSize(%p)\n", carrier);
 	int i;
 	int ret = 0;
@@ -177,3 +189,139 @@ static int getPayloadSize(BYTE *carrier) {
 	return ret;
 }
 
+
+/*********************************************************************/
+/**************************  LSB4  ***********************************/
+/*********************************************************************/
+
+static stegResult_t stegEmbedLSB4(dataHolder_t *carrier, dataHolder_t *payload, char *extention) {
+	LOG("stegEmbedLSB4((%p, %d), (%p, %d), %s)\n", carrier->data, carrier->size, payload->data, payload->size, STR_NULL(extention));
+	long bits = BITS_PER_BYTE*payload->size;
+	LOG("data bits: %ld\n", bits);
+
+	writePayloadSizeLSB4(carrier->data, payload->size);
+
+	writePayloadDataLSB4(carrier->data, sizeof(DWORD)*BITS_PER_BYTE,  payload->data, bits);
+
+	if(extention != NULL){
+		writePayloadDataLSB4(carrier->data, (sizeof(DWORD) + payload->size)*BITS_PER_BYTE,(BYTE*) extention, (strlen(extention)+1)*BITS_PER_BYTE);
+	}
+
+	return stegResult_Success;
+}
+
+static stegResult_t stegExtractLSB4(dataHolder_t *carrier, dataHolder_t *payload, char *extention) {
+	int sizeOffset = BITS_PER_BYTE * sizeof(DWORD);
+		long i;
+		int size;
+
+
+		LOG("reading payloadSize\n");
+		size = getPayloadSizeLSB4(carrier->data);
+		payload->size = size;
+		LOG("payload size: %d\n", size);
+
+		if ((payload->data = (BYTE*) malloc(size)) == NULL) {
+			return stegResult_memoryFail;
+		}
+
+		for (i = 0; i < size ; i++) {
+			payload->data[i] <<= 4;
+			payload->data[i] |= bitArrayGetFourLeastSignificant(carrier->data[(i+sizeOffset)*BLOCK_SIZE+1], i);
+		}
+
+		if(extention != NULL){
+			int offset = (sizeof(DWORD)+payload->size)*BITS_PER_BYTE;
+			char val;
+			i = 0;
+			if((val = getByteLSB(&offset, carrier->data)) != '.'){
+				WARN("Looking for extention, but '.' is not there...\n");
+				free(payload->data);
+				payload->data = NULL;
+				return stegResult_fail;
+			} else {
+				extention[i++] = val;
+				do {
+					val = getByteLSB(&offset, carrier->data);
+					extention[i++] = val;
+				}while(val != 0 && i < MAX_FILE_EXTENTION);
+
+				if(val != 0){
+					WARN("Not found Null termination in extention\n");
+					free(payload->data);
+					payload->data = NULL;
+					return stegResult_fail;
+				}
+			}
+
+		}
+
+		return stegResult_Success;
+}
+
+static void writePayloadSizeLSB4(BYTE *carrier, int size){
+	LOG("writePayloadSizeLSB4(%p, %d)\n", carrier, size);
+	int i;
+
+	for(i = 0 ; i < sizeof(DWORD) ; i++){
+
+		// clear the 4 least significant bits of the block
+		carrier[(2*i)*BLOCK_SIZE+1] &= ~0xF;
+
+		// set the 4 least significant bits of the block with the four most significative from size
+		carrier[(2*i)*BLOCK_SIZE+1] |= getFourMostSignificantDWORD(size, i);
+
+		// clear the 4 least significant bits of the block
+		carrier[(2*i+1)*BLOCK_SIZE+1] &= ~0xF;
+
+		// set the 4 least significant bits of the block with the four least significative from size
+		carrier[(2*i+1)*BLOCK_SIZE+1] |= getFourLeastSignificantDWORD(size, i);
+	}
+}
+
+static void writePayloadDataLSB4(BYTE *carrier, int offset, BYTE *payload, long bits) {
+	LOG("writePayloadData(%p, %d, %p, %ld)\n", carrier, offset, payload, bits);
+	LOG("sizeOffset: %d\n", offset);
+	long i;
+
+	for (i = 0; i < bits / BITS_PER_BYTE; i++) {
+
+		// clear 4 least significant bits
+		carrier[((2*i)+offset)*BLOCK_SIZE+1] &= ~0xF;
+
+		// clear 4 most significant bits
+		carrier[((2*i)+offset)*BLOCK_SIZE+1] |= bitArrayGetFourMostSignificant(carrier, ((2*i)+offset)*BLOCK_SIZE+1);
+
+		// clear 4 least significant bits
+		carrier[((2*i+1)+offset)*BLOCK_SIZE+1] &= ~0xF;
+
+		// clear 4 most significant bits
+		carrier[((2*i+1)+offset)*BLOCK_SIZE+1] |= bitArrayGetFourLeastSignificant(carrier, ((2*i+1)+offset)*BLOCK_SIZE+1);
+	}
+}
+
+static int getPayloadSizeLSB4(BYTE *carrier) {
+	LOG("getPayloadSizeLSB4(%p)\n", carrier);
+	int i;
+	unsigned int ret = 0;
+
+	for(i = 0 ; i < sizeof(DWORD); i++){
+		ret <<= 4;
+		ret |= getFourLeastSignificantDWORD(carrier, i);
+	}
+
+	return ret;
+}
+
+static char getByteLSB4(int *offset, BYTE *carrier) {
+	char ret = 0;
+	int bits = sizeof(char)*BITS_PER_BYTE;
+	int i;
+	for(i = 0 ; i < bits ; i++){
+		if(carrier[(i+*offset)*BLOCK_SIZE+1] & 0x1){
+			bitArraySetBYTE((BYTE*)&ret, i, 1);
+		}
+	}
+	*offset += i;
+	return ret;
+}
