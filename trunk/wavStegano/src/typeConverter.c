f458a6c9f846322715a8ba1920d9ae2aae9b01e2
anizzomc <anizzomc@6d05fb35-34e8-c98c-94b2-7deb9cabda7c>
2011-06-07 00:57:21 +0000

    Parece que funciona, hay q probarlo ahora....

--- trunk/wavStegano/src/typeConverter.c
+++ trunk/wavStegano/src/typeConverter.c
@@ -1,26 +1,38 @@
 /* typeConverter.c */
 
 #include <string.h>
+#include "types.h"
 #include "typeConverter.h"
 
 #define DEBUG_LOG
 #include "debug.h"
 
+void reverseInt(intConvert_t *number);
+
 void typeConvertFCtoDH(fileContainer_t  *fileContainer, dataHolder_t *dataHolder){
+	intConvert_t org;
 	LOG("typeConvertFCtoDH((fileContainer{%d, %p, %s}, dataHolder{%d, %p})\n", fileContainer->size, fileContainer->rawData, fileContainer->extension, dataHolder->size, dataHolder->data);
+	org.number = fileContainer->size;
+	reverseInt(&org);
+
 	dataHolder->size = sizeof(int) + fileContainer->size + strlen(fileContainer->extension);
 	LOG("dataHolder->size=%d\n", dataHolder->size);
 	dataHolder->data = malloc(dataHolder->size);
-	((int *)dataHolder->data)[0] = fileContainer->size;
+	((int *)dataHolder->data)[0] = org.number;
 	memcpy(dataHolder->data + sizeof(int), fileContainer->rawData, fileContainer->size);
 	memcpy(dataHolder->data + sizeof(int) + fileContainer->size, fileContainer->extension, strlen(fileContainer->extension));
 
 }
 
 void typeConvertDHtoFC(fileContainer_t  *fileContainer, dataHolder_t *dataHolder){
+	intConvert_t org;
+
+
 	LOG("typeConvertDHtoFC((fileContainer{%d, %p, %s}, dataHolder{%d, %p})\n", fileContainer->size, fileContainer->rawData, fileContainer->extension, dataHolder->size, dataHolder->data);
 	int extSize;
-	fileContainer->size = ((int *)dataHolder->data)[0];
+	org.number = ((int *)dataHolder->data)[0];
+	reverseInt(&org);
+	fileContainer->size = org.number;
 	LOG("fileSize is: %d\n", fileContainer->size);
 	fileContainer->rawData = malloc(fileContainer->size);
 	extSize = dataHolder->size - (fileContainer->size + sizeof(int));
@@ -29,6 +41,23 @@ void typeConvertDHtoFC(fileContainer_t  *fileContainer, dataHolder_t *dataHolder
 	memcpy(fileContainer->extension, dataHolder->data + sizeof(int) + fileContainer->size, extSize);
 	fileContainer->extension[extSize] = 0;
 	LOG("ext is %s\n", fileContainer->extension);
+
+}
+
+void reverseInt(intConvert_t *number){
+	char aux;
+
+	printf("%d\n", number->number);
+
+	aux = number->bytes[3];
+	number->bytes[3] = number->bytes[0];
+	number->bytes[0] = aux;
+
+	aux = number->bytes[1];
+	number->bytes[1] = number->bytes[2];
+	number->bytes[2] = aux;
+
+	printf("%d\n", number->number);
 
 }
 
